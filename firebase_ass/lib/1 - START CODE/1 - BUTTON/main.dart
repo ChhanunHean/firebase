5f88b2d62bf3a0710bd1b1d7e182b6ec00cd7360
ChhanunHean <chhanunhean@gmail.com>
2026-07-09 22:55:24 +0700

    firebase

--- firebase_ass/lib/1 - START CODE/1 - BUTTON/main.dart
+++ firebase_ass/lib/1 - START CODE/1 - BUTTON/main.dart
@@ -1,10 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-// -----------------
-// Model
-// -----------------
 class ButtonStatus {
   final String name;
   final bool selected;
